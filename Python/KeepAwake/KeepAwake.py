d0ba2a9a60ba8b5e040ac1fb632be6c5fb271571
ShiftyMaverick <84161832+ShiftyMaverick@users.noreply.github.com>
2021-06-04 12:56:39 +1000

    Update

--- Python/KeepAwake/KeepAwake.py
+++ Python/KeepAwake/KeepAwake.py
@@ -2,7 +2,7 @@ import keyboard,time,datetime,os,sys
 from datetime import datetime
 from os import system
 
-system("title "+"KeepAwake ")
+system("title "+"KeepAwake - By: Anthony ")
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++
 #VARIABLES BELOW
